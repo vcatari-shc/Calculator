3633bdfacdfaaed79d9b3b7aa6f1f0a7c75caa9d
vardhini catari <vardhini.catari@gmail.com>
2017-02-18 17:37:03 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+ function add(){
+      document.getElementById('result').innerHTML = '+' 
+      console.log('add');
+    }
+ function substract(){
+      document.getElementById('result').innerHTML = '-' 
+      console.log('substract');
+    }
+ function multiply(){
+      document.getElementById('result').innerHTML = '*' 
+      console.log('multiply');
+    }
+ function divide(){
+      document.getElementById('result').innerHTML = '/' 
+      console.log('divide');
+    }
